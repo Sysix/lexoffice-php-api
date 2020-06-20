caa71e2400984c99ab6372faa59da2ac5cba14c7
Alexander S <sysix@sysix-coding.de>
2020-06-20 15:53:38 +0200

    add VoucherClient Test

---
+++ tests/src/Voucher/ClientTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Src\Voucher;
+
+use Clicksports\LexOffice\Voucher\Client;
+use GuzzleHttp\Psr7\Response;
+use Tests\TestClient;
+
+class ClientTest extends TestClient
+{
+
+    public function testCreate()
+    {
+        $stub  = $this->createClientMockObject(
+            Client::class,
+            new Response(200, [], 'body'),
+            ['create']
+        );
+
+        $response = $stub->create([
+            'version' => 0
+        ]);
+
+        $this->assertEquals('body', $response->getBody()->__toString());
+    }
+
+    public function testGet()
+    {
+        $stub  = $this->createClientMockObject(
+            Client::class,
+            new Response(200, [], 'body'),
+            ['get']
+        );
+
+        $response = $stub->get('abc');
+
+        $this->assertEquals('body', $response->getBody()->__toString());
+    }
+
+    public function testGetAll()
+    {
+        $stub  = $this->createClientMockObject(
+            Client::class,
+            new Response(200, [], '{"content": [], "totalPages": 1}'),
+            ['getAll']
+        );
+
+        $response = $stub->getAll();
+
+        $this->assertEquals('{"content": [], "totalPages": 1}', $response->getBody()->__toString());
+    }
+
+    public function testUpdate()
+    {
+        $stub  = $this->createClientMockObject(
+            Client::class,
+            new Response(200, [], '{}'),
+            ['update']
+        );
+
+        $response = $stub->update('resource-id', []);
+
+        $this->assertEquals('{}', $response->getBody()->__toString());
+    }
+}
