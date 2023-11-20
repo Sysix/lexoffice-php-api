26faced5ecdc9c79dbc5552b938b0a68e2562515
Alexander S <sysix@sysix-coding.de>
2023-11-20 18:03:32 +0100

    add update method for contact client, add missing tests for contact cleint

--- src/Contact/Client.php
+++ src/Contact/Client.php
@@ -22,4 +22,20 @@ class Client extends PaginationClient
             '&direction=' . $this->sortDirection .
             '&property=' .$this->sortProperty;
     }
+
+    /**
+    * @param string $id
+    * @param array $data
+    * @return ResponseInterface
+    * @throws CacheException
+    * @throws LexOfficeApiException
+    */
+    public function update(string $id, array $data)
+    {
+        $api = $this->api->newRequest('PUT', $this->resource . '/' . $id);
+
+        $api->request = $api->request->withBody($this->createStream($data));
+
+        return $api->getResponse();
+    }
 }
