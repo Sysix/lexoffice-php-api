7d558a9ef0289eaba752120f1701e17074da7701
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-10-16 09:33:16 +0200

    CS

--- tests/Clients/DunningTest.php
+++ tests/Clients/DunningTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Sysix\LexOffice\Tests\Clients;
 
-use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Sysix\LexOffice\Clients\Dunning;
 use Sysix\LexOffice\Tests\TestClient;
